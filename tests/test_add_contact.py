5f98d56148829f60ab176ac94c5058be3872631b
Vafelence <vafelence@yandex.ru>
2021-09-10 00:39:05 +0300

    Все тесты создания, удаления, модификации для групп и контактов Задание 7

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,15 +1,7 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.contact import Contact
-from fixture.application import Application
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
-    
 def test_create_new_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create_contact(Contact(firstname="joe", middlename="barbaro", address="russia", mobile="nokia",
