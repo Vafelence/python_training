2db426de1a839839ed5296d19b165286a834a27d
Vafelence <vafelence@yandex.ru>
2021-09-11 15:24:40 +0300

    Блок 3. Задание 8. Проверка валидности фикстуры и проверка состояния сессии

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -3,8 +3,8 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.group.create_contact(Group(name="123", header="123", footer="123"))
+    app.group.create_group(Group(name="123", header="123", footer="123"))
 
 
 def test_add_empty_group(app):
-    app.group.create_contact(Group(name="", header="", footer=""))
+    app.group.create_group(Group(name="", header="", footer=""))
