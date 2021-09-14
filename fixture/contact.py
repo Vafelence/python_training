cb04f82da250f67bc36caca0cb58991419b48021
Vafelence <vafelence@yandex.ru>
2021-09-14 14:57:00 +0300

    Блок 3. Задание 8

--- fixture/contact.py
+++ fixture/contact.py
@@ -99,7 +99,6 @@ class ContactHelper:
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(contact.notes)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        wd.find_element_by_link_text("Logout").click()
 
     def delete_first_contact(self):
         wd = self.app.wd
